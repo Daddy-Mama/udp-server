89c10cdbc9c9598c45a4b7e81f4d60f0d1e8d613
ArtemKoliada <artem.kolyada@almexoft.com>
2020-11-27 16:54:47 +0200

    add logging system remove useless

--- src/main/java/com/udp/server/udpserver/contoller/WebSocketController.java
+++ src/main/java/com/udp/server/udpserver/contoller/WebSocketController.java
@@ -2,18 +2,15 @@ package com.udp.server.udpserver.contoller;
 
 import com.udp.server.udpserver.dto.Greeting;
 import com.udp.server.udpserver.dto.HelloMessage;
-import com.udp.server.udpserver.dto.WSMessageDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+
 
 @Controller
 public class WebSocketController {
-    @Autowired
-    private SimpMessagingTemplate template;
 
 
     @MessageMapping("/maniac-home-room/{room}")
